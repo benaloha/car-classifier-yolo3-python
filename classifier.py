e6a084cf564eead30a95ae26b94d8adccf009cc1
Spectrico <48276857+spectrico@users.noreply.github.com>
2020-09-01 16:36:48 +0300

    Add files via upload

--- classifier.py
+++ classifier.py
@@ -3,7 +3,8 @@
 
 import numpy as np
 import json
-import tensorflow as tf
+#import tensorflow as tf
+import tensorflow.compat.v1 as tf
 from PIL import Image, ImageOps
 import cv2
 import io
@@ -34,6 +35,46 @@ def load_labels(label_file):
 
     return label
 
+def resizeAndPad(img, size, padColor=0):
+    h, w = img.shape[:2]
+    sh, sw = size
+
+    # interpolation method
+    if h > sh or w > sw: # shrinking image
+        interp = cv2.INTER_AREA
+    else: # stretching image
+        interp = cv2.INTER_CUBIC
+
+    # aspect ratio of image
+    aspect = w/h  # if on Python 2, you might need to cast as a float: float(w)/h
+
+    # compute scaling and pad sizing
+    if aspect > 1: # horizontal image
+        new_w = sw
+        new_h = np.round(new_w/aspect).astype(int)
+        pad_vert = (sh-new_h)/2
+        pad_top, pad_bot = np.floor(pad_vert).astype(int), np.ceil(pad_vert).astype(int)
+        pad_left, pad_right = 0, 0
+    elif aspect < 1: # vertical image
+        new_h = sh
+        new_w = np.round(new_h*aspect).astype(int)
+        pad_horz = (sw-new_w)/2
+        pad_left, pad_right = np.floor(pad_horz).astype(int), np.ceil(pad_horz).astype(int)
+        pad_top, pad_bot = 0, 0
+    else: # square image
+        new_h, new_w = sh, sw
+        pad_left, pad_right, pad_top, pad_bot = 0, 0, 0, 0
+
+    # set pad color
+    if len(img.shape) is 3 and not isinstance(padColor, (list, tuple, np.ndarray)): # color image but only one color provided
+        padColor = [padColor]*3
+
+    # scale and pad
+    scaled_img = cv2.resize(img, (new_w, new_h), interpolation=interp)
+    scaled_img = cv2.copyMakeBorder(scaled_img, pad_top, pad_bot, pad_left, pad_right, borderType=cv2.BORDER_CONSTANT, value=padColor)
+
+    return scaled_img
+
 class Classifier():
     def __init__(self):
         # uncomment the next 3 lines if you want to use CPU instead of GPU
@@ -54,12 +95,7 @@ class Classifier():
 
     def predict(self, img):
         img = img[:, :, ::-1]
-        h, w = img.shape[:2]
-        center_crop_size = min(w, h)
-        x = int((w - center_crop_size) / 2)
-        y = int((h - center_crop_size) / 2)
-        img = img[y:y + center_crop_size, x:x + center_crop_size]
-        img = cv2.resize(img, classifier_input_size)
+        img = resizeAndPad(img, classifier_input_size)
 
         # Add a forth dimension since Tensorflow expects a list of images
         img = np.expand_dims(img, axis=0)
